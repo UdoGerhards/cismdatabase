d34a9b379a8e7070a1596a2c2a36df71a3e836fb
UdoGerhards <Udo.Gerhards@gerhards.eu>
2026-02-25 18:17:20 +0100

    Config changed back

--- config.js
+++ config.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-export const DB_STRING =
-  "mongodb+srv://udogerhards:oQ9cvFIvVVWsXMS7@cimsexams.zcovvz2.mongodb.net/?appName=cimsexams";
-=======
-const DB_STRING =
-
->>>>>>> 694e40fcbe467bd6ed42cc047e2c723424194290
+export const DB_STRING =  "mongodb+srv://udogerhards:oQ9cvFIvVVWsXMS7@cimsexams.zcovvz2.mongodb.net/?appName=cimsexams";
 
 export const DB_TEST_NAME = "cism_test";
 

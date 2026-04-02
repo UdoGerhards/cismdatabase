3b3e5b23f82dfd1f664807b380bb9a5d23229fd7
UdoGerhards <Udo.Gerhards@gerhards.eu>
2026-04-02 17:54:56 +0200

    Second version of Authenticator App registration

---
+++ configuration/passwordRules.js
@@ -0,0 +1,9 @@
+// config/passwordRules.js
+export const PASSWORD_RULES = {
+  minLength: 8,
+  maxLength: 64,
+  requireUppercase: true,
+  requireLowercase: true,
+  requireNumber: true,
+  requireSpecialChar: true,
+};

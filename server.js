6edf85a50dd8ad55c5384203c0329e7968269abc
UdoGerhards <Udo.Gerhards@gerhards.eu>
2026-06-29 09:01:34 +0200

    Added telgram message on startup

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 import { DB_CONNECTION, DB_NAME } from "#config";
+import TelegramBot from "node-telegram-bot-api";
 import Server from "./lib/server/routes.js";
 
 const server = new Server(DB_NAME, DB_CONNECTION);
 server.init();
-server.listen(3000);
+//server.listen(3000);
+
+// Ersetze diese Werte durch deine Daten
+const token = process.env.TELEGRAM_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
+
+const bot = new TelegramBot(token, { polling: false });
+
+function sendStartupNotification() {
+  const message = `🚀 *CISMDatabase gestartet!*\n\nDie Anwendung wurde erfolgreich hochgefahren.\nZeitpunkt: ${new Date().toLocaleString()}`;
+
+  bot
+    .sendMessage(chatId, message, { parse_mode: "Markdown" })
+    .then(() => console.log("Start-Benachrichtigung gesendet."))
+    .catch((err) => console.error("Fehler beim Senden:", err));
+}
+server.listen(3000, sendStartupNotification());
